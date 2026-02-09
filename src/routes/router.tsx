b529b3650a2273fb40ce4091b91d27f0fce58122
Jonnathan23 <jsaquicela26@gmail.com>
2026-02-08 23:32:27 -0500

    router actualizado

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -7,6 +7,10 @@ import RegisterPage from "../features/security/pages/Register.page";
 import DashboardPage from "../core/pages/DashboardPage";
 import RolesPage from "../features/security/pages/Roles.page";
 import UserPage from "../features/security/pages/User.page";
+import CategoriesPage from "../features/clients/pages/CategoriesPage";
+import ProductsPage from "../features/clients/pages/ProductsPage";
+import ProvidersPage from "../features/providers/pages/ProvidersPage";
+import PurchasesPage from "../features/providers/pages/PurchasesPage";
 
 
 export default function Router() {
@@ -16,8 +20,15 @@ export default function Router() {
                 <Route element={<AppLayout />}>
                     <Route path="/" element={<DashboardPage />} />
                     <Route path="/auth/register-new-user" element={<RegisterPage />} />
+
                     <Route path="/roles" element={<RolesPage />} />
                     <Route path="/users" element={<UserPage />} />
+
+                    <Route path="/categories" element={<CategoriesPage />} />
+                    <Route path="/products" element={<ProductsPage />} />
+
+                    <Route path="/providers" element={<ProvidersPage />} />
+                    <Route path="/purchases" element={<PurchasesPage />} />
                 </Route>
             </Routes>
             <Routes>
